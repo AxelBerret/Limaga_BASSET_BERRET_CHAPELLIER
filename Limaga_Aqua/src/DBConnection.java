de6db276438d8e3fd90b8b3604b0cdb8f5c24119
Axel Berret <axelberret6@etu.univ-lorraine.fr>
2023-12-11 10:59:47 +0100

    Creation mini-projet

---
+++ Limaga_Aqua/src/DBConnection.java
@@ -0,0 +1,64 @@
+import java.sql.*;
+import java.util.Properties;
+
+public class DBConnection {
+
+    private String userName;
+    private String password;
+    private String serverName;
+    private String portNumber;
+    private static String dbName;
+    private static Connection connection;
+
+
+    /**
+     * Constructeur
+     * @throws SQLException
+     */
+    private DBConnection() throws SQLException {
+        //initialisation des attributs
+        this.userName="root";
+        this.password="";
+        this.serverName="localhost";
+        this.portNumber="3307";
+        this.dbName = "limaga";
+
+        // creation de la connection
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
+        String urlDB = "jdbc:mysql://" + serverName + ":";
+        urlDB += portNumber + "/" + dbName;
+
+        //maj de la connection
+        DBConnection.connection = DriverManager.getConnection(urlDB, connectionProps);
+    }
+
+
+    /**
+     * méthode getConnection
+     * @return
+     * @throws SQLException
+     */
+    public static synchronized Connection getConnection() throws SQLException {
+        if(DBConnection.connection==null)
+            new DBConnection();
+        return DBConnection.connection;
+    }
+
+    /**
+     * méthode setNomDB
+     * @param nomDB
+     * @throws SQLException
+     */
+    public static void setNomDB(String nomDB) throws SQLException {
+        if(dbName!=null&&!nomDB.equals(dbName)){
+            dbName=nomDB;
+            connection=null;
+        }
+    }
+
+    public static String getNomDB() {
+        return dbName;
+    }
+}
